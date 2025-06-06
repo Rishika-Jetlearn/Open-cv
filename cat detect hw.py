f08d259db51205900720d150d02dd82f90f9044f
Rishika-Jetlearn <vreakiwala.vk@gmail.com>
2025-06-06 15:54:06 +0100

    cat detect hw.py

---
+++ cat detect hw.py
@@ -0,0 +1,7 @@
+import cv2
+
+no_cat=cv2.imread("landscape.jpg",cv2.IMREAD_COLOR)
+no_cat2=cv2.imread("snow.jpg",cv2.IMREAD_COLOR)
+cat=cv2.imread("",cv2.IMREAD_COLOR)
+cat2=cv2.imread("",cv2.IMREAD_COLOR)
+cat3=cv2.imread("",cv2.IMREAD_COLOR)
