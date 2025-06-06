c34c16cb95830111b4c527fa3b5560b92bf4a20d
Rishika-Jetlearn <vreakiwala.vk@gmail.com>
2025-06-06 18:01:48 +0100

    cat detect hw.py

--- cat detect hw.py
+++ cat detect hw.py
@@ -1,7 +1,18 @@
 import cv2
 
-no_cat=cv2.imread("landscape.jpg",cv2.IMREAD_COLOR)
-no_cat2=cv2.imread("snow.jpg",cv2.IMREAD_COLOR)
-cat=cv2.imread("",cv2.IMREAD_COLOR)
-cat2=cv2.imread("",cv2.IMREAD_COLOR)
-cat3=cv2.imread("",cv2.IMREAD_COLOR)
+
+xml=r"C:\Users\Freze\Jetlearn Python projects-Rishika\Open cv\Open-cv\cat xml.xml"
+classifier=cv2.CascadeClassifier(xml)
+vid="catvideo.mp4"
+video=cv2.VideoCapture(vid)
+while video.isOpened():
+    b,f=video.read()
+    f=cv2.resize(f,(1000,800))
+    if b==False:
+        continue
+    grey=cv2.cvtColor(f,cv2.COLOR_BGR2GRAY)
+    list_cats=classifier.detectMultiScale(grey,1.3,10)
+    for x,y,w,h in list_cats:
+        cv2.rectangle(f,(x,y),(x+w,y+h),(255,0,0),3)
+    cv2.imshow("cat",f)
+    cv2.waitKey(1000)
