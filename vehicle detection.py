b2f4d382e7aa4c951ec56e201488ecf8a1d7b4f4
Rishika-Jetlearn <vreakiwala.vk@gmail.com>
2025-06-13 17:47:38 +0100

    vehicle detection.py

--- vehicle detection.py
+++ vehicle detection.py
@@ -1,16 +1,17 @@
 import cv2
 
-xml=
+xml=r"C:\Users\Freze\Jetlearn Python projects-Rishika\Open cv\Open-cv\cars.xml"
 classifier=cv2.CascadeClassifier(xml)
-vid_file=""
+vid_file="video.avi"
 video=cv2.VideoCapture(vid_file)
 while video.isOpened():
     b,f=video.read()
     if b==False:
         continue
-    grey_vid=cv2.cvtColor(f,cv2.COLOR_BGR2GRAY)
-    list_cats=classifier.detectMultiScale(grey,1.3,10)
+    resized=cv2.resize(f,(1000,800))
+    grey_vid=cv2.cvtColor(resized,cv2.COLOR_BGR2GRAY)
+    list_vehicles=classifier.detectMultiScale(grey_vid,1.1,5)
     for x,y,w,h in list_vehicles:
-        cv2.rectangle(f,(x,y),(x+w,y+h),(255,0,0),3)
-    cv2.imshow("vehicle detection",f)
-    cv2.waitKey(1000)
+        cv2.rectangle(resized,(x,y),(x+w,y+h),(255,0,0),3)
+    cv2.imshow("vehicle detection",resized)
+    cv2.waitKey(10)
